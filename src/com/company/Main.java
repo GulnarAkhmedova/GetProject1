fb95daaf0ce39d98066e1062a614b448c6435e7b
elnar <elnarasabira@gmal.com>
2022-05-14 11:12:30 -0700

    the updeted commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        //teamlead project
         System.out.println("I am Groot!!!");
         System.out.println("I will get this correctly!!");
         System.out.println("i am Groot3!!!");
 
         System.out.println("I am correct code");
         System.out.println("I am not confused!");
+        System.out.println("heloo my friend");
     }
 }
 
